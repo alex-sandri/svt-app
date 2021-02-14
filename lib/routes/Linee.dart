e5ed471c0ac6f9c7208c37f03b4244c7fe231a02
Alex Sandri <me@alexsandri.com>
2021-02-14 16:21:14 +0100

    Create route 'Linee'

---
+++ lib/routes/Linee.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Linee extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Container(),
+      ),
+    );
+  }
+}
