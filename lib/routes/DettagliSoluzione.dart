e63cfa726e097aa9a77f499ea00427abe74b4d1a
Alex Sandri <me@alexsandri.com>
2021-02-17 16:50:05 +0100

    Show solution details

---
+++ lib/routes/DettagliSoluzione.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:svt_app/models/SoluzioneDiViaggio.dart';
+import 'package:svt_app/widgets/SvtAppBar.dart';
+
+class DettagliSoluzione extends StatelessWidget {
+  final SoluzioneDiViaggio soluzione;
+
+  DettagliSoluzione(this.soluzione);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        appBar: SvtAppBar(),
+        body: ListView(
+          padding: const EdgeInsets.all(10),
+          children: [
+            Text(
+              "Dettaglio soluzione",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+            ListTile(
+              leading: Icon(Icons.trip_origin),
+              title: Text(DateFormat.Hm().format(soluzione.oraPartenza)),
+              subtitle: Text(soluzione.localitaSalita),
+            ),
+            ListTile(
+              leading: Icon(Icons.place),
+              title: Text(DateFormat.Hm().format(soluzione.oraArrivo)),
+              subtitle: Text(soluzione.localitaDiscesa),
+            ),
+            ListTile(
+              isThreeLine: true,
+              leading: Icon(Icons.schedule),
+              title: Text("${soluzione.minutiTotali} minuti"),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("${soluzione.minutiPiedi} a piedi"),
+                  Text("${soluzione.minutiBordo} a bordo"),
+                ],
+              ),
+            ),
+            ListTile(
+              isThreeLine: true,
+              leading: Icon(Icons.directions_walk),
+              title: Text("${soluzione.metriTotali} metri"),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("${soluzione.metriPiedi} a piedi"),
+                  Text("${soluzione.metriBordo} a bordo"),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
