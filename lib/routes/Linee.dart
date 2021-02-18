ad4625de2843a38cb2653bd5d2b860fc610a3ef8
Alex Sandri <me@alexsandri.com>
2021-02-18 15:51:01 +0100

    Add search input

--- lib/routes/Linee.dart
+++ lib/routes/Linee.dart
@@ -11,46 +11,59 @@ class Linee extends StatelessWidget {
     return Material(
       child: Scaffold(
         appBar: SvtAppBar(),
-        body: StreamBuilder<List<Linea>>(
-          stream: Api.ottieniLinee(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return Loading();
-            }
+        body: GestureDetector(
+          behavior: HitTestBehavior.translucent,
+          onTap: FocusScope.of(context).unfocus,
+          child: StreamBuilder<List<Linea>>(
+            stream: Api.ottieniLinee(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return Loading();
+              }
 
-            return ListView(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Text(
-                    "Linee",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
+              return ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      "Linee",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index) => ListTile(
-                    title: snapshot.data[index].titolo,
-                    subtitle: snapshot.data[index].sottotitlo,
-                    isThreeLine: true,
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => LocalitaView(snapshot.data[index]),
-                        ),
-                      );
-                    },
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: "Cerca",
+                        prefixIcon: Icon(Icons.search),
+                      ),
+                    ),
                   ),
-                ),
-              ],
-            );
-          },
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) => ListTile(
+                      title: snapshot.data[index].titolo,
+                      subtitle: snapshot.data[index].sottotitlo,
+                      isThreeLine: true,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LocalitaView(snapshot.data[index]),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
